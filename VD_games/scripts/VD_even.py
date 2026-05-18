528add656de24dc6a875c901d1d27f57353bef76
t777832-source <t777832@gmail.com>
2026-05-18 18:48:06 +0300

    PR#4

--- VD_games/scripts/VD_even.py
+++ VD_games/scripts/VD_even.py
@@ -3,25 +3,25 @@ import prompt
 
 
 def main():
-print("Welcome to the VD-games!")
+    print("Welcome to the VD-games!")
 
-name = prompt.string("May I have your name? ")
-print(f"Hello, {name}!")
-print('Answer "yes" if the number is even, otherwise answer "no".')
-correct_answers = 0
-while correct_answers < 3:
-number = random.randint(1, 100)
-print(f"Question: {number}")
-answer = prompt.string("Your answer: ")
-correct_answer = "yes" if number % 2 == 0 else "no"
-if answer == correct_answer:
-print("Correct!")
-correct_answers += 1
-else:
-print(
-f"'{answer}' is wrong answer ;(. "
-f"Correct answer was '{correct_answer}'."
-)
-print(f"Let's try again, {name}!")
-return
-print(f"Congratulations, {name}!")
+    name = prompt.string("May I have your name? ")
+    print(f"Hello, {name}!")
+    print('Answer "yes" if the number is even, otherwise answer "no".')
+    correct_answers = 0
+    while correct_answers < 3:
+        number = random.randint(1, 100)
+        print(f"Question: {number}")
+        answer = prompt.string("Your answer: ")
+        correct_answer = "yes" if number % 2 == 0 else "no"
+        if answer == correct_answer:
+            print("Correct!")
+            correct_answers += 1
+        else:
+            print(
+            f"'{answer}' is wrong answer ;(. "
+            f"Correct answer was '{correct_answer}'."
+            )
+            print(f"Let's try again, {name}!")
+            return
+    print(f"Congratulations, {name}!")
